b471c5be9f8fc817cd759b2174bff66250a62a50
James Ball <hello@james-ball.co.uk>
2018-03-07 15:22:34 +0000

    Misc Commit #4

--- dev/markdown.lisp
+++ dev/markdown.lisp
@@ -2,3 +2,11 @@
   (break-list-at ; Strip content after second ---
     (cdr ; Remove first ---
       (split '(#\return #\linefeed) file)) "---"))
+
+(defun lisp-md-header (_file_)
+  (map 'list (lambda (pairs)
+                     (setf pairs (split '(#\:) pairs))
+                     (map 'list (lambda (pair)
+                                        (setf pair (string-trim " " pair)))
+                                        pairs))
+                     (parse-md-header _file_)))
--- main.lisp
+++ main.lisp
@@ -15,14 +15,5 @@
 (load-local "util/string")
 (load-local "dev/markdown")
 
-(defvar file
+(defvar *file*
   (file-contents "~/Development/lisp/data/page.md"))
-
-; (parse-md-header file)
-
-; (split '(#\:) "key: value")
-; (string-trim " " "  key: value  ")
-
-(map 'list (lambda (s)
-             (split '(#\:) s)) (parse-md-header file))
-
