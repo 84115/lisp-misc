54c5e819b1d37b6e9c3234cb0eb5645f367f60bb
James Ball <hello@james-ball.co.uk>
2018-03-07 13:28:30 +0000

    Misc Commit #2

--- main.lisp
+++ main.lisp
@@ -11,14 +11,16 @@
   (load-local "main"))
 
 (load-local "util/io")
-(load-local "util/math")
+; (load-local "util/math")
 (load-local "util/string")
 (load-local "dev/markdown")
-
-; (print
-;    (split '(#\space) "my name is james"))
 
 (defvar file
   (file-contents "~/Development/lisp/data/page.md"))
 
 ; (parse-md-header file)
+
+(split '(#\:) "key: value")
+(string-trim " " "  key: value  ")
+
+(map 'list (lambda (x) (+ x 10)) '(1 2 3 4))
